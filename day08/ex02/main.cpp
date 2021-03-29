0d4c120ff6a945f289e59b78f1b366f03e161127
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-03-29 14:58:02 -0400

    doublr main

--- day08/ex02/main.cpp
+++ day08/ex02/main.cpp
@@ -1,27 +1,58 @@
-#include "mutantstack.hpp"
+# include "mutantstack.hpp"
+# include <iostream>
+# include <list>
 
 int main()
 {
+	std::cout << "### Subject Test ###" << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
+	std::cout << "Number on top of the stack:\t";
 	std::cout << mstack.top() << std::endl;
 	mstack.pop();
+	std::cout << "Size after remove the top:\t";
 	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
+	std::cout << "The first element of the stack:\t" << *it << std::endl;
+	std::cout << "The last element of the stack:\t" << *(--ite) << std::endl;
+	ite++;
+	std::cout << "Display what is on the stack:\t" ;
 	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		std::cout << *it << " ";
 		++it;
 	}
+	std::cout << std::endl << std::endl;
 	std::stack<int> s(mstack);
-	return 0;
+
+	std::cout << "### List Test ###" << std::endl;
+	std::list<int> mlist;
+	mlist.push_back(5);
+	mlist.push_back(17);
+	std::cout << "Number on top of the stack:\t";
+	std::cout << mlist.back() << std::endl;
+	mlist.pop_back();
+	std::cout << "Size after remove the top:\t";
+	std::cout << mlist.size() << std::endl;
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	mlist.push_back(0);
+	std::list<int>::iterator it1 = mlist.begin();
+	std::list<int>::iterator ite1 = mlist.end();
+	std::cout << "The first element of the stack:\t" << *it1 << std::endl;
+	std::cout << "The last element of the stack:\t" << *(--ite1) << std::endl;
+	ite1++;
+	std::cout << "Display what is on the stack:\t" ;
+	while (it1 != ite1)
+	{
+		std::cout << *it1 << " ";
+		++it1;
+	}
 }
